24b785034447e7b90f1514aa51ba82d7cc280972
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2024-10-03 10:18:24 +0700

    JDBC: listStudents() => show table

--- src/main/java/com/example/java3n_crud_sd19306/repository/StudentDao.java
+++ src/main/java/com/example/java3n_crud_sd19306/repository/StudentDao.java
@@ -1,22 +1,70 @@
 package com.example.java3n_crud_sd19306.repository;
 
 import com.example.java3n_crud_sd19306.entity.Student;
+import com.example.java3n_crud_sd19306.jdbc.DatabaseConnectionManager;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class StudentDao {
 
+    private static DatabaseConnectionManager dcm =
+            new DatabaseConnectionManager("test1", "sa", "123456");
+
     // Fake data
     private static ArrayList<Student> students = new ArrayList<>();
-    static {
-        students.add(new Student(1001L, "student 1", "email 1", "phone 1"));
-        students.add(new Student(1002L, "student 2", "email 2", "phone 2"));
-        students.add(new Student(1003L, "student 3", "email 3", "phone 3"));
-    }
+    //static {
+    //    students.add(new Student(1001L, "student 1", "email 1", "phone 1"));
+    //    students.add(new Student(1002L, "student 2", "email 2", "phone 2"));
+    //    students.add(new Student(1003L, "student 3", "email 3", "phone 3"));
+    //}
 
     public ArrayList<Student> getStudents() {
 
-        return students;
+        ArrayList<Student> students1 = new ArrayList<>();
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+
+            connection = dcm.getConnection();
+            String sql = """
+                        SELECT * FROM students;
+                    """;
+
+            preparedStatement = connection.prepareStatement(sql);
+            //preparedStatement.setLong(1, 1001L);
+            //preparedStatement.setString(2, "student 1");
+            //preparedStatement.setString(3, "email 1");
+            //preparedStatement.setString(4, "phone 1");
+
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+
+                Student student = new Student();
+                student.setId(resultSet.getLong("id"));
+                student.setName(resultSet.getString("name"));
+                student.setEmail(resultSet.getString("email"));
+                student.setPhone(resultSet.getString("phone"));
+
+                students1.add(student);
+            }
+
+            System.out.println("done...");
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            dcm.close(resultSet, preparedStatement, connection);
+        }
+
+        return students1;
     }
 
     public void deleteStudent(Long id) {
